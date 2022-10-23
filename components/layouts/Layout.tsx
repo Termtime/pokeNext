cab293fc94175708f079cdacb3f9af46abe549cf
Mario Mejía <mariofermejia99@gmail.com>
2022-10-22 19:06:45 -0600

    feat: add Open Graph Meta tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import {useRouter} from "next/router";
 import {Navbar} from "../ui/Navbar";
 
 interface LayoutProps {
@@ -6,20 +7,33 @@ interface LayoutProps {
   title?: string;
 }
 
+/**
+ * Origin (Base URL) of the application.
+ *
+ * This is needed because if this layout is rendered on the server, `window.location.origin`
+ * will give an error, as `window` is undefined.
+ */
+const origin = typeof window === "undefined" ? "" : window.location.origin;
+
 export const Layout = ({children, title}: LayoutProps) => {
   return (
     <>
       <Head>
         <title>{title || "PokéNext App"}</title>
         <meta name="author" content="Mario Mejía" />
-        <meta
-          name="description"
-          content={`Informacion sobre el pokemon: ${title}`}
-        />
+        <meta name="description" content={`Information about ${title}`} />
         <meta
           name="keywords"
           content={`"${title}, pokedex, pokemon, pokemon app"`}
         />
+
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content={`Information about ${title}`} />
+        <meta
+          property="og:description"
+          content={`This is the page about ${title}`}
+        />
+        <meta property="og:image" content={`${origin}/img/banner.png`} />
       </Head>
 
       <Navbar />
